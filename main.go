d3aa073553f9e7e7603eefad535dede6fcd8ccc8
braheezy <michael.c.braha@gmail.com>
2024-04-17 19:32:01 -0700

    Documentation and cleanup

--- main.go
+++ main.go
@@ -12,61 +12,64 @@ import (
 )
 
 var (
-	otoCtx *oto.Context
+	// The global player. Should it be global? ¯\_(ツ)_/¯
 	player *oto.Player
 )
 
 func main() {
-	c := make(chan struct{})
+	// Declare functions that can be called from js.
 	js.Global().Set("loadWav", js.FuncOf(loadWav))
 	js.Global().Set("playWav", js.FuncOf(playWav))
 	js.Global().Set("pauseWav", js.FuncOf(pauseWav))
-	<-c // Keep the Go runtime alive
+	// Block forever, to keep Go runtime alive.
+	select {}
 }
 
+// loadWav loads a WAV file into memory.
 func loadWav(this js.Value, args []js.Value) interface{} {
+	// Create the Oto options with sane defaults.
 	op := &oto.NewContextOptions{
 		SampleRate:   44100,
 		ChannelCount: 2,
 		Format:       oto.FormatSignedInt16LE,
 	}
-	var err error
-	var ready chan struct{}
-	otoCtx, ready, err = oto.NewContext(op)
+	// Create the Oto context.
+	otoCtx, ready, err := oto.NewContext(op)
 	if err != nil {
 		return map[string]interface{}{"error": err.Error()}
 	}
+	// Wait for the context to be ready. Must be in a goroutine or things deadlock.
 	go func() {
 		<-ready
 	}()
+	// Read WAV data from js.
 	data := make([]byte, args[0].Get("byteLength").Int())
 	js.CopyBytesToGo(data, args[0])
-
+	// Decode the WAV data.
 	r := bytes.NewReader(data)
 	decoder := wav.NewDecoder(r)
 	if !decoder.IsValidFile() {
 		return map[string]interface{}{"error": "invalid WAV file"}
 	}
-
 	buf, err := decoder.FullPCMBuffer()
 	if err != nil {
 		return map[string]interface{}{"error": "failed to read WAV buffer: " + err.Error()}
 	}
-
+	// Convert to bytes.
 	byteData := make([]byte, len(buf.Data)*2)
 	for i, sample := range buf.Data {
 		binary.LittleEndian.PutUint16(byteData[i*2:], uint16(sample))
 	}
-
-	// Calculate duration and call setAudioDuration in JavaScript
+	// Once we can figure out the duration of the audio, we can tell the UI about it.
 	durationMs := float64(len(buf.Data)) / float64(decoder.Format().SampleRate) * 1000 / float64(decoder.Format().NumChannels)
 	js.Global().Call("setAudioDuration", durationMs)
-
+	// Finally, load the data into the player.
 	player = otoCtx.NewPlayer(bytes.NewReader(byteData))
 
 	return nil
 }
 
+// pauseWav pauses the audio player.
 func pauseWav(this js.Value, args []js.Value) interface{} {
 	if player != nil {
 		player.Pause()
@@ -74,6 +77,7 @@ func pauseWav(this js.Value, args []js.Value) interface{} {
 	return nil
 }
 
+// playWav plays the audio player.
 func playWav(this js.Value, args []js.Value) interface{} {
 	if player != nil {
 		player.Play()
